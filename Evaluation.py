83e93c2c221fc06e8f4f331d513b452725193bd3
David Yu <pppsdavid@gmail.com>
2018-10-26 00:01:11 -0400

    Update Evaluation.py to a functional level

--- Evaluation.py
+++ Evaluation.py
@@ -1,38 +1,76 @@
-"""
-The algorithum below is aiming at providing a 2-layer neural network based evaluation to the Reversi AI.
-"""
 import numpy as np
-class EvaluationNetwork:
 
-    def __init__ (self,x,y,c):
+
+@np.vectorize
+def sigmoid(x):
+    return 1 / (1 + np.e ** -x)
+
+
+activation_function = sigmoid
+from scipy.stats import truncnorm
+
+
+def truncated_normal(mean=0, sd=1, low=0, upp=10):
+    return truncnorm(
+        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+
+
+class NeuralNetwork:
+
+    def __init__(self,
+                 no_of_in_nodes,
+                 no_of_out_nodes,
+                 no_of_hidden_nodes,
+                 learning_rate):
+        self.no_of_in_nodes = no_of_in_nodes
+        self.no_of_out_nodes = no_of_out_nodes
+        self.no_of_hidden_nodes = no_of_hidden_nodes
+        self.learning_rate = learning_rate
+        self.create_weight_matrices()
+
+    def create_weight_matrices(self):
         """
-        :param x: the input vector (all the board)
-        :param y: the expected output vector, one dimensional, the win ratio
-        :param c:the learning rate, 0.1-0.01
+        A method to initialize the weight matrices of the neural network
         """
-        self.input = x
-        self.weight_ih = np.random.rand(self.input.shape[1],100)
-        self.weight_ho = np.random.rand(100,1)
-        self.y = y
-        self.output = np.zeros(y.shape)
-        self.c = c
+        rad = 1 / np.sqrt(self.no_of_in_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+        self.weights_in_hidden = X.rvs((self.no_of_hidden_nodes,
+                                        self.no_of_in_nodes))
+        rad = 1 / np.sqrt(self.no_of_hidden_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+        self.weights_hidden_out = X.rvs((self.no_of_out_nodes,
+                                         self.no_of_hidden_nodes))
 
-    def feed_forward(self):
-        self.hidden_layer = sigmoid(np.dot(self.input,self.weight_ih))
-        #Update the value of the hidden layer
-        self.output = sigmoid(np.dot(self.hidden_layer,self.weight_ho))
-        #Update the output layer value
+    def train(self, input_vector, target_vector):
+        # input_vector and target_vector can be tuple, list or ndarray
 
-    def backprop(self):
-        d_weight_ho = np.dot(self.hidden_layer.T, (2*(self.y-self.output)*sigmoid_derivative(self.output)))
-        d_weight_ih = np.dot(self.input.T, (np.dot(2*(self.y - self.output)* sigmoid_derivative(self.output) , self.weight_ho.T) \
-                                            * sigmoid_derivative(self.hidden_layer)))
-        #update the weight using gradiant decend
+        input_vector = np.array(input_vector, ndmin=2).T
+        target_vector = np.array(target_vector, ndmin=2).T
 
-        self.weight_ih += self.c*d_weight_ih
-        self.weight_ho += self.c*d_weight_ho
+        output_vector1 = np.dot(self.weights_in_hidden, input_vector)
+        output_vector_hidden = activation_function(output_vector1)
 
-if __name__ == '__main__':
-    test = EvaluationNetwork([1,1,1],0,0.1)
-    test.feed_forward()
-    print('{},{}'.format(test.outputtest.y))
+        output_vector2 = np.dot(self.weights_hidden_out, output_vector_hidden)
+        output_vector_network = activation_function(output_vector2)
+
+        output_errors = target_vector - output_vector_network
+        # update the weights:
+        tmp = output_errors * output_vector_network * (1.0 - output_vector_network)
+        tmp = self.learning_rate * np.dot(tmp, output_vector_hidden.T)
+        self.weights_hidden_out += tmp
+        # calculate hidden errors:
+        hidden_errors = np.dot(self.weights_hidden_out.T, output_errors)
+        # update the weights:
+        tmp = hidden_errors * output_vector_hidden * (1.0 - output_vector_hidden)
+        self.weights_in_hidden += self.learning_rate * np.dot(tmp, input_vector.T)
+
+    def run(self, input_vector):
+        # input_vector can be tuple, list or ndarray
+        input_vector = np.array(input_vector, ndmin=2).T
+        output_vector = np.dot(self.weights_in_hidden, input_vector)
+        output_vector = activation_function(output_vector)
+
+        output_vector = np.dot(self.weights_hidden_out, output_vector)
+        output_vector = activation_function(output_vector)
+
+        return output_vector
